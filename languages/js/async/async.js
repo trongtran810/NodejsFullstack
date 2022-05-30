47a6a38b0035196afcb17b090fb6df782d33f3f0
Trong Tran <gacontuyenchien1@gmail.com>
2022-05-30 11:26:44 +0700

    3005 add more test for async js

--- languages/js/async/async.js
+++ languages/js/async/async.js
@@ -1,4 +1,4 @@
-async function fooSubAcsync(x){
+let fooSubAcsync = async(x) => {
     console.log(`hello from subacsync ${x}`);
     for (let i = 1; i < 10; i++){
         console.log(`async ${i}`);
@@ -6,7 +6,7 @@ async function fooSubAcsync(x){
     return 1;
 }
 
-async function fooAcsync(x) {
+let fooAcsync = async(x) => {
     console.log(`hello from acsync ${x}`);
     for (let i = 1; i < 10; i++){
         console.log(`async ${i}`);
@@ -15,14 +15,14 @@ async function fooAcsync(x) {
     return 1;
 }
 
-function fooSubPromise(x) {
+let fooSubPromise = (x) => {
     console.log(`hello from subpromise ${x}`);
     for (let i = 1; i < 10; i++){
         console.log(`async ${i}`);
     }
     return Promise.resolve(1);
 }
-function fooPromise(x) {
+let fooPromise = (x) => {
     console.log(`hello from promise ${x}`);
     for (let i = 1; i < 10; i++){
         console.log(`async ${i}`);
@@ -31,9 +31,35 @@ function fooPromise(x) {
     return Promise.resolve(1);
 }
 
-for (let i=1; i< 100; i++){
-    let a = fooAcsync(i);
-    console.log(`parent for loop ${a}`);
-    let b = fooPromise(i);
-    console.log(`parent for loop ${b}`);
+function test1(){
+    for (let i=1; i< 2; i++){
+        let a = fooAcsync(i);
+        console.log(`parent for loop ${a}`);
+        let b = fooPromise(i);
+        console.log(`parent for loop ${b}`);
+    }
 }
+
+// Test 2
+let promise1 = new Promise((myResolve)=>{
+    console.log("promise1 - 1");
+    myResolve("Hello");
+    console.log("promise1 - 2");
+});
+
+let callback1 = ( inFunc ) => {
+    console.log("callback - 1");
+    let x = 1;
+    let val = inFunc(x);
+    console.log(`value ${val}`);
+    console.log("callback - 2");
+}
+
+console.log("==============> Begin test 2");
+function test2(){
+    promise1.then( (val)=> console.log(`Test2: return from promise1: ${val}`) );
+    callback1( (x) => x+1 );
+}
+
+test1();
+// test2();
